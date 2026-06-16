9e19de77433bb2c73e850cd369db89086f2fa1c9
Lui Cardoso <luiza6cardoso@gmail.com>
2026-06-16 17:45:24 -0300

    adicionei codigo da função editar registro

--- trabalho-apc1/trabalho-luiza-cardoso.c
+++ trabalho-apc1/trabalho-luiza-cardoso.c
@@ -14,7 +14,7 @@ int main() {
     // printf("2 - Listar registros\n");
     // printf("3 - Buscar registro\n");
     // printf("4 - Atualizar registro\n\n");
-    // printf("Selecione uma opção: \n");
+    // printf("Selecione uma opção: \n"); //ADD seleçao invalida
 
     struct filme_t {
         char titulo[51];
@@ -30,16 +30,17 @@ int main() {
         printf("Insira os dados do Filme %d:\n", i+1);
         
         puts("Título:");
-        scanf("[^\n]s", filmes[i].titulo);
+        scanf("%50[^\n]s", filmes[i].titulo);
         while(getchar() != '\n');
 
         puts("Ano:");
         scanf("%d", &filmes[i].ano);
         while(getchar() != '\n');
 
-        puts("Estrelas (1 a 5):");
+        puts("Estrelas (1 a 5):"); //ADD numero <1 ou >5 invalido
         scanf("%d", &filmes[i].estrelas);
         while(getchar() != '\n');
+        printf("\n");
     }
     printf("\n");
 
@@ -52,6 +53,40 @@ int main() {
         printf("Estrelas: %d\n\n", filmes[i].estrelas);
     }
 
+    //EDITAR REGISTRO
+
+    int num;
+    puts("Digite o número do filme que deseja editar:");
+    scanf("%d", &num);
+    while(getchar() != '\n');
+    int num_edit = num-1; // numero real que será editado
+    // ADD if num > TAM é invalido
+
+    char titulo_novo[51];
+    int ano_novo, estrelas_novo;
+
+    printf("Insira os dados do filme %d\n", num);
+    puts("Título:");
+    scanf("%50[^\n]s", &titulo_novo);
+    while(getchar() != '\n');
+
+    puts("Ano:");
+    scanf("%d", &ano_novo);
+    while(getchar() != '\n');
+
+    puts("Estrelas (1 a 5):");
+    scanf("%d", &estrelas_novo);
+    while(getchar() != '\n');
+
+    strcpy(filmes[num_edit].titulo, titulo_novo);
+    filmes[num_edit].ano = ano_novo;
+    filmes[num_edit].estrelas = estrelas_novo;
+
+    printf("\n");
+    printf("Filme %d\n", num);
+    printf("Título: %s\n", filmes[num_edit].titulo);
+    printf("Ano: %d\n", filmes[num_edit].ano);
+    printf("Estrelas: %d\n\n", filmes[num_edit].estrelas);
 
     return 0;
 }
